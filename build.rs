f9254a4089d832719299b1e3aafd16c875be7724
Steven Dee (Jōshin) <mrdomino@stairwell.com>
2025-03-15 17:05:29 -0400

    Override default settings with build.rs (#23)
    This lets different organizations more easily use this tool by making
    the default remote name and credential helper binary name configurable
    at build time.

---
+++ build.rs
@@ -0,0 +1,13 @@
+use std::env;
+
+fn main() {
+    let default_remote = env::var("ASPECT_REMOTE")
+        .unwrap_or_else(|_| "aw-remote-ext.buildremote.stairwell.io".into());
+    let default_helper = env::var("ASPECT_CREDENTIAL_HELPER")
+        .unwrap_or_else(|_| "aspect-credential-helper".into());
+
+    println!("cargo::rerun-if-env-changed=ASPECT_REMOTE");
+    println!("cargo::rerun-if-env-changed=ASPECT_CREDENTIAL_HELPER");
+    println!("cargo::rustc-env=ASPECT_REMOTE={}", default_remote);
+    println!("cargo::rustc-env=ASPECT_CREDENTIAL_HELPER={}", default_helper);
+}
